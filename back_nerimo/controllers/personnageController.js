6fccdcbe86b2844a93547fdbe1e7ba75b91df2b4
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-04-03 16:07:17 +0200

    création d'un personnage si l'ID de la planète existe

--- back_nerimo/controllers/personnageController.js
+++ back_nerimo/controllers/personnageController.js
@@ -1,20 +1,40 @@
-import Personnage from "../models/Personnage.js";
+import Personnage from '../models/Personnage.js';
+import Planete from '../models/Planete.js'; 
 
 export async function createPersonnage(req, res) {
     try {
-      const personnage = new Personnage(req.body);
-      await personnage.save();
-      res.status(201).json({ message: 'Personnage créée', data: personnage });
+        const planeteExiste = await Planete.exists({ _id: req.body.planeteRef });
+
+        if (!planeteExiste) {
+            return res.status(400).json({ message: 'La planète spécifiée n\'existe pas.' });
+        }
+
+        const personnage = new Personnage(req.body);
+        await personnage.save();
+        res.status(201).json({ message: 'Personnage créée', data: personnage });
+
     } catch (error) {
-      res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message });
     }
+}
+
+export async function getPersonnages(req, res) {
+  try {
+    const personnages = await Personnage.find().populate("planeteRef").exec();
+    res.status(200).json({ message: 'Liste des personnages', data: personnages });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  
-  export async function getPersonnages(req, res) {
-    try {
-      const personnages = await Personnage.find();
-      res.status(200).json({ message: 'Liste des personnages', data: personnages });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+}
+
+export async function deletePersonnage(req, res) {
+  try {
+    const deletedPersonnage = await Personnage.findByIdAndDelete(req.params.id);
+    if (!deletedPersonnage) {
+      return res.status(404).json({ message: "Personnage non trouvé" });
     }
+    res.status(200).json({ message: "Personnage supprimé", data: deletedPersonnage });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
+}
--- back_nerimo/routes/personnageRoutes.js
+++ back_nerimo/routes/personnageRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createPersonnage, getPersonnages } from '../controllers/personnageController.js';
+import { createPersonnage, getPersonnages, deletePersonnage } from '../controllers/personnageController.js';
 
 const router = express.Router();
 
 router.post('/', createPersonnage);
 router.get('/', getPersonnages);
+router.delete('/:id', deletePersonnage);
 
 export default router;
