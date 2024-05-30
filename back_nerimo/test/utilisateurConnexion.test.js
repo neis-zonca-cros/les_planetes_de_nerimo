2df2e759f662c201c4ba4c8e5066c555c89a6f24
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-05-30 11:02:11 +0200

    test connexion et création utilisateur

---
+++ back_nerimo/test/utilisateurConnexion.test.js
@@ -0,0 +1,100 @@
+import { describe, it, expect, beforeEach, afterAll } from 'vitest';
+import request from 'supertest';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import utilisateurRoutes from '../routes/utilisateurRoutes.js';
+import Utilisateur from '../models/Utilisateur.js'; 
+
+dotenv.config({ path: '.env.test' });
+
+const app = express();
+app.use(express.json());
+app.use('/api/utilisateur', utilisateurRoutes());
+
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`connecté à la base de données ${process.env.MONGODB_URI}`),
+  console.log(`connecté sur le port ${process.env.PORT}`),
+
+  await Utilisateur.deleteMany({});
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+describe('POST /api/utilisateur/connexion', () => {
+  it('Retourner une erreur 401 si le mail est invalide', async () => {
+    const response = await request(app)
+      .post('/api/utilisateur/connexion')
+      .send({ email: 'invalid@example.com', mdp: 'password' });
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Compte invalide');
+  });
+
+  it('Retourne une erreur 401 sur le mdp est invalide', async () => {
+    const utilisateur = new Utilisateur({
+    prenom:'Néïs test',
+      email: 'valid@example.com',
+      mdp: await bcrypt.hash('password', 10),
+      tentativeConnexion: 0,
+    });
+    await utilisateur.save();
+
+    const response = await request(app)
+      .post('/api/utilisateur/connexion')
+      .send({ email: 'valid@example.com', mdp: 'invalidpassword' });
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Compte invalide');
+  });
+
+  it('Retourne 200 si tout est ok pour la connexion utilisateur', async () => {
+    const utilisateur = new Utilisateur({
+        prenom:"Néïs",
+      email: 'valid@example.com',
+      mdp: await bcrypt.hash('password', 10),
+      tentativeConnexion: 0,
+    });
+    await utilisateur.save();
+
+    const response = await request(app)
+      .post('/api/utilisateur/connexion')
+      .send({ email: 'valid@example.com', mdp: 'password' });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Connexion réussie');
+    expect(response.body.token).toBeDefined();
+    
+    const decoded = jwt.verify(response.body.token, process.env.JWT_SECRET);
+    expect(decoded.email).toBe(utilisateur.email);
+  });
+
+  it('Retourne une erreur 401 si le compte est bloqué', async () => {
+    const utilisateur = new Utilisateur({
+        prenom:'Néïs',
+      email: 'valid@example.com',
+      mdp: await bcrypt.hash('password', 10),
+      tentativeConnexion: 5,
+      dateBlocageConnexion: new Date(Date.now() - 3000),
+    });
+    await utilisateur.save();
+
+    const response = await request(app)
+      .post('/api/utilisateur/connexion')
+      .send({ email: 'valid@example.com', mdp: 'password' });
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Compte bloqué. Réessayez après 5 minutes.');
+  });
+
+
+});
---
+++ back_nerimo/test/utilisateurCreation.test.js
@@ -0,0 +1,90 @@
+import { describe, it, expect, beforeEach, afterAll } from 'vitest';
+import request from 'supertest';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import utilisateurRoutes from '../routes/utilisateurRoutes.js';
+import Utilisateur from '../models/Utilisateur.js'; 
+import sinon from 'sinon';
+
+dotenv.config({ path: '.env.test' });
+
+const app = express();
+app.use(express.json());
+app.use('/api/utilisateur', utilisateurRoutes());
+
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`connecté à la base de données ${process.env.MONGODB_URI}`),
+  console.log(`connecté sur le port ${process.env.PORT}`),
+
+  await Utilisateur.deleteMany({});
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+describe('POST /api/utilisateur/creer', () => {
+
+  
+    it('Retourne 403 si lutilisateur se met en admin à la création de son compte', async () => {
+        const response = await request(app)
+          .post('/api/utilisateur/creer')
+          .send({ email: 'valid@example.com', mdp: 'validpassword', mdp_repeat:'validpassword', prenom: 'Néïs', admin: true });
+      
+        expect(response.status).toBe(403);
+        expect(response.body).toHaveProperty('error', 'Permission refusée de créer un utilisateur administrateur');
+      });
+
+
+      it('Renvoi une erreur 500 si le mdp ne se hash pas', async () => {
+        
+        const bcryptStub = sinon.stub(bcrypt, 'hash').rejects(new Error('Erreur de hachage'));
+      
+        const response = await request(app)
+          .post('/api/utilisateur/creer')
+          .send({ email: 'valid@example.com', mdp: 'validpassword', mdp_repeat: 'validpassword', prenom: 'Néïs', admin: false });
+      
+        expect(response.status).toBe(500); 
+        expect(response.body).toHaveProperty('error', 'Erreur de hachage');
+      
+        
+        bcryptStub.restore();
+      });
+
+      it('Renvoie une erreur 500 si le mail nest pas au bon format', async () => {
+        const response = await request(app)
+          .post('/api/utilisateur/creer')
+          .send({ email: 'invalid_email', mdp: 'validpassword', mdp_repeat: 'validpassword', prenom: 'Néïs', admin: false });
+      
+        expect(response.status).toBe(500); 
+        
+      });
+  
+      it('Retourne 201 si la création du compte est ok avec hash du mdp', async () => {
+        const email = 'valid@example.com';
+        const password = 'password';
+        const response = await request(app)
+          .post('/api/utilisateur/creer')
+          .send({ email, mdp: password, mdp_repeat: password, prenom: 'Néïs' });
+      
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe('Utilisateur créé');
+      
+        const utilisateur = await Utilisateur.findOne({ email });
+      
+        const isPasswordHashed = await bcrypt.compare(password, utilisateur.mdp);
+        expect(isPasswordHashed).toBe(true);
+      });
+      
+  
+  
+  });
+  
