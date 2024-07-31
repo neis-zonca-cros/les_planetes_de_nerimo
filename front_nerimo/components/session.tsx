9afd3965bf83586c09fe24e041229bd907cafbc9
Néïs <ncros@sqli.com>
2024-07-31 14:30:12 +0200

    session UI

--- front_nerimo/components/session.tsx
+++ front_nerimo/components/session.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     marginHorizontal:10,
     alignItems: 'center', 
     justifyContent: 'center',
-    marginVertical:5,
+    marginVertical:10,
   },
   
 });
