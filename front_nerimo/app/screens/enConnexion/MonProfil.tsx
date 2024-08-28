1d366c75c979e09c2df9a25549a01e45088326e7
Néïs <ncros@sqli.com>
2024-08-28 11:55:47 +0200

    mes informations

--- front_nerimo/app/screens/enConnexion/MonProfil.tsx
+++ front_nerimo/app/screens/enConnexion/MonProfil.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Alert } from "react-native";
+import { View, Alert, StyleSheet } from "react-native";
 import { useTheme } from "@/app/hooks/themeContext";
 import TopBar from "@/app/components/TopBar";
 import { Dimensions } from "react-native";
@@ -9,6 +9,8 @@ import { ThemedStyles } from "@/app/utils/styles";
 import { RootStackParamList } from "@/app/types";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
+import { ProfilRondIcon } from "@/app/assets/icons/profilRondIcon";
+import ProfilItems from "@/app/components/profilItems";
 
 
 type MonProfilScreen = StackNavigationProp<RootStackParamList, "MonProfil">;
@@ -16,17 +18,76 @@ type MonProfilScreen = StackNavigationProp<RootStackParamList, "MonProfil">;
 const MonProfil: React.FC = () => {
   const navigation = useNavigation<MonProfilScreen>();
   const { theme } = useTheme();
-  const { utilisateur } = useUser();
+  const { utilisateur, deconnexion } = useUser();
+  const screenHeight = Dimensions.get("window").height;
   const goBack = useGoBack();
   const styleTheme = ThemedStyles(theme);
 
+  const handleLogout = async () => {
+    try {
+      await deconnexion();
+      navigation.navigate("Bienvenue");
+    } catch (error) {
+      console.error("Erreur lors de la déconnexion:", error);
+    }
+  };
+
+  function toggleMusic(value: boolean): void {
+    if (value) {
+      Alert.alert('Switch musique activé !', 'Le switch est maintenant activé.');
+    } else {
+      Alert.alert('Switch musique désactivé !', 'Le switch est maintenant désactivé.');
+    };
+  }
+  const goToMesInformations = async()=> {
+    navigation.navigate("MesInformations")
+  }
+
   return (
     <View style={styleTheme.container}>
-      <TopBar titre="Informations de" prenom={utilisateur?.prenom} iconeDroiteAction={goBack} iconeDroiteNom={"close-outline"} />
-
+      <TopBar titre="Profil de" prenom={utilisateur?.prenom} iconeDroiteAction={goBack} iconeDroiteNom={"close-outline"} />
+      <View style={styles.mainContainer}>
+        <View style={styles.leftContainer}>
+          <View style={theme.colors.effectShadow}>
+            <ProfilRondIcon
+              width={screenHeight * 0.25}
+              fill={theme.colors.primaryButton}
+              background={theme.colors.background}
+            />
+          </View>
+        </View>
+        <View style={styles.rightContainer}>
+          <View style={styles.containerBoutton}>
+            <ProfilItems text="Mes informations" iconName="arrow-forward-outline" onPress={goToMesInformations}></ProfilItems>
+            <ProfilItems text="Musique" isSwitch={true} onSwitchToggle={toggleMusic}></ProfilItems>
+            <ProfilItems text="Lightmode" isSwitch={true}></ProfilItems>
+            <ProfilItems text="Partir" iconName="log-out-sharp" onPress={handleLogout}></ProfilItems>
+          </View></View>
+      </View>
     </View>
   );
 };
 export default MonProfil;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  leftContainer: {
+    flex: 0.40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rightContainer: {
+    flex: 0.60,
+    justifyContent: "center",
+    paddingTop: 20,
+  },
+  containerBoutton: {
+    width: '100%',
+    alignItems: 'flex-start'
+  }
+});
 
 
