1a4d8b1f5fb30a955750da21406b69029f6e6927
Néïs <ncros@sqli.com>
2024-08-29 15:57:39 +0200

    fetch update utilisateur

--- front_nerimo/app/services/utilisateurFetch.ts
+++ front_nerimo/app/services/utilisateurFetch.ts
@@ -88,3 +88,56 @@ export async function creerUtilisateur(
   }
 }
 
+interface UpdateUserRequest {
+  prenom?: string;
+  email?: string;
+  mdp?: string;
+  mdp_repeat?: string;
+}
+
+interface UpdateUserResponse {
+  data: {
+    _id: string;
+    prenom: string;
+    email: string;
+  };
+  message: string;
+}
+
+export async function updateUtilisateur(
+  utilisateurId: string,
+  userData: UpdateUserRequest
+): Promise<UpdateUserResponse["data"]> {
+  try {
+    const { token } = await getCredentials();
+    if (!token) {
+      throw new Error("Token non trouvé");
+    }
+
+    const response = await apiFetch<UpdateUserResponse>(`utilisateur/${utilisateurId}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: userData, 
+    });
+
+    if (!response || !response.data) {
+      throw new Error("Réponse de l'API invalide");
+    }
+
+    console.log("Réponse du backend après mise à jour de l'utilisateur :", response);
+
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors de la mise à jour de l'utilisateur:", error);
+
+    if (error.message === "Les mots de passe ne correspondent pas" || error.statusCode === 400) {
+      Alert.alert("Erreur de mise à jour", "Les mots de passe ne correspondent pas ou d'autres erreurs se sont produites.");
+    } else {
+      Alert.alert("Erreur", "Une erreur s'est produite lors de la mise à jour des informations. Veuillez réessayer.");
+    }
+
+    throw error;
+  }
+}
